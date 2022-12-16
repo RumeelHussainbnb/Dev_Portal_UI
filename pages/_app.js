0e8708067c2f6689c62a7a638a5403df3e1bf0df
RumeelHussainbnb <rumeelhussainbnb@gmail.com>
2022-12-16 21:20:27 +0500

    Include Google Tag Manager Script

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../style.css';
-
+import Script from 'next/script';
 import { AppWrapper } from '../context/AppContext';
 
 import { Web3ReactProvider } from '@web3-react/core'
@@ -13,6 +13,15 @@ function getLibrary(provider) {
 export default function App({ Component, pageProps }) {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <Script id="google-taag-manager" strategy="afterInteractive">
+          {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5PHXRNS');
+          `}
+        </Script>
         <Web3ReactProvider getLibrary={getLibrary}>
           <AppWrapper>
             <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -14,10 +14,26 @@ export default function Document() {
           href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.28.0/themes/prism-tomorrow.min.css"
           rel="stylesheet"
         />
+        <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5PHXRNS');`,
+        }}
+        />
       </Head>
+
       <body>
         <Main />
         <NextScript />
+        <noscript
+    dangerouslySetInnerHTML={{
+      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5PHXRNS"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+    }}
+  />
       </body>
     </Html>
   );
