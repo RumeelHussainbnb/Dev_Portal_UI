593381ad9ee153abeb959a4a2a7656d5cf12c1f3
RumeelHussainbnb <rumeelhussainbnb@gmail.com>
2022-12-22 15:00:10 +0500

    Added GA4 Tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
 import '../style.css';
-
 import { AppWrapper } from '../context/AppContext';
-
 import { Web3ReactProvider } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers'
+import Script from 'next/script';
 
 function getLibrary(provider) {
   const library = new Web3Provider(provider)
@@ -13,6 +12,22 @@ function getLibrary(provider) {
 export default function App({ Component, pageProps }) {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-YJHYYK44RJ`} />
+
+      <Script strategy="lazyOnload"
+       id='google-analytics'
+       dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-YJHYYK44RJ', {
+          page_path: window.location.pathname,
+        });
+        `,
+        }}
+      />
+
         <Web3ReactProvider getLibrary={getLibrary}>
           <AppWrapper>
             <Component {...pageProps} />
